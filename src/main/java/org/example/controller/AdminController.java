c6e6d4e344c55922641abd07da8754673e04804b
Konstantin <k.bobkov1408@gmail.com>
2024-04-18 18:37:34 +0300

    base admin panel

---
+++ src/main/java/org/example/controller/AdminController.java
@@ -0,0 +1,14 @@
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/admin")
+@Controller
+public class AdminController {
+    @GetMapping()
+    public String adminMainPage(){
+        return "admin";
+    }
+}
