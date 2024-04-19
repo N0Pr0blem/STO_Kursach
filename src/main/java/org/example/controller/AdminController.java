a3923d608724d1636af7dedaad8a6d16e3c4e986
Konstantin <k.bobkov1408@gmail.com>
2024-04-19 12:41:16 +0300

    Create services

--- src/main/java/org/example/controller/AdminController.java
+++ src/main/java/org/example/controller/AdminController.java
@@ -1,14 +1,27 @@
 package org.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/admin")
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
     @GetMapping()
-    public String adminMainPage(){
+    public String mainPage(){
         return "admin";
+    }
+    @GetMapping("/service")
+    public String servicePage(Model model){
+        return "admin_service";
+    }
+    @GetMapping("/category")
+    public String categoryPage(Model model){
+        return "admin_category";
+    }
+    @GetMapping("/history")
+    public String historyPage(Model model){
+        return "admin_history";
     }
 }
